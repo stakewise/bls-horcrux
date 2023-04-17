131986bd177b24584945119748b0e5ca0c8202b8
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2023-04-17 11:11:57 +0300

    Add batch sign

--- cli/sign.py
+++ cli/sign.py
@@ -9,6 +9,20 @@ from cli.crypto import HorcruxPbkdf2Keystore
 
 @click.command()
 @click.option(
+    "--payloads-file",
+    required=False,
+    help="The path to the file containing signing payloads. Defaults to ./payloads.json",
+    default='./payloads.json',
+    type=click.Path(exists=True, file_okay=True, dir_okay=False),
+)
+@click.option(
+    "--output-file",
+    required=False,
+    help="The file to save signatures to. Defaults to ./output.json.",
+    default="./output.json",
+    type=click.Path(exists=False, file_okay=True, dir_okay=False),
+)
+@click.option(
     "--horcrux-file",
     prompt="Enter the path to the horcrux keystore",
     help="The path to the horcrux keystore",
@@ -22,7 +36,7 @@ from cli.crypto import HorcruxPbkdf2Keystore
         for your password as otherwise it will appear in your shell history.)""",
     prompt="Enter the horcrux password used during your horcrux creation",
 )
-def sign(horcrux_file: str, horcrux_password: str) -> None:
+def sign(payloads_file: str, output_file: str, horcrux_file: str, horcrux_password: str) -> None:
     """Unlocks the keystore and signs the data."""
     horcrux_file = horcrux_file.strip()
     if not os.path.exists(horcrux_file):
@@ -32,17 +46,23 @@ def sign(horcrux_file: str, horcrux_password: str) -> None:
     with open(horcrux_file, "r") as key_file:
         keystore = HorcruxPbkdf2Keystore.create_from_json(json.load(key_file))
 
-    signing_data = click.prompt(
-        text="Enter hexadecimal encoded data to sign", type=click.STRING
-    ).strip()
-    if signing_data.startswith("0x"):
-        signing_data = signing_data[2:]
+    click.echo(f"Loading payloads from {payloads_file}...")
+    with open(payloads_file, "r") as f:
+        payloads = json.load(f)
 
     # decrypt and sign data
     private_key = int.from_bytes(keystore.decrypt(horcrux_password), "big")
-    signature = bls_pop.Sign(private_key, bytes.fromhex(signing_data))
 
-    click.echo(f"Signature: {click.style(f'0x{signature.hex()}', fg='green')}")
+    click.echo(f"Signing payloads...")
+    signatures = {}
+    for index, payload in payloads.items():
+        signature = bls_pop.Sign(private_key, bytes.fromhex(payload))
+        signatures[index] = signature.hex()
+
+    with open(output_file, "w") as f:
+        json.dump(signatures, f)
+
+    click.echo(f"Signatures saved to {click.style(output_file, fg='green')}...")
     click.echo(f"Horcrux index: {click.style(f'{keystore.index}', fg='green')}")
     click.echo(
         f"""Next steps:
